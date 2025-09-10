67ec45b326cf1b82611d9d3987531d910a5a93c8
Daniele Oliveti <daniele@MacBook-Air-di-Daniele.local>
2025-09-10 18:07:52 +0200

    modifica

--- delphi/config.py
+++ delphi/config.py
@@ -215,3 +215,6 @@ class RunConfig(Serializable):
 
     """List of run stages to recompute. This is a debugging tool
     and may be removed in the future."""
+
+    cache_only: bool = False
+    """If True, only populate the latent cache and exit (skip neighbours, explanations, and scoring)."""
